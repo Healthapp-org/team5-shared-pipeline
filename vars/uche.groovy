c7e84a1e9f94b3776b7b805c5cb7dd98965c847b
Pogun147 <105657287+Pogun147@users.noreply.github.com>
2022-06-14 08:49:46 -0400

    Update uche.groovy

--- vars/uche.groovy
+++ vars/uche.groovy
@@ -1,17 +1,42 @@
 def call(String repoUrl){
-pipeline {
-       agent any
-       tools {
-           maven 'mymaven'
-       }
-       stages {
-           stage("Tools initialization") {
-               steps {
-                   sh 'mvn --version'
-                   sh 'java -version'
-               }
-           }
-           stage("Checkout Code") {
+pipeline{
+  agent any
+  stages{
+    stage('version-control'){
+        steps{
+            checkout([$class: 'GitSCM', branches: [[name: '*/main']], extensions: [], userRemoteConfigs: [[credentialsId: 'f7c765a1-e95f-44ad-9f53-7b9fc6ad20ec', url: 'https://github.com/Healthapp-org/team5A1.git']]])
+        }
+    }
+    stage('parallel-job'){
+      parallel{
+        stage('sub-job1'){
+          steps{
+            echo 'simpletest.sh'
+          }
+        }
+        stage('sub-job2'){
+          steps{
+            sh 'lscpu'
+          }
+        }
+        stage('sub-job3'){
+          steps{
+            sh 'uptime'
+          }
+        }
+        stage('sub-job4'){
+          steps{
+            sh 'ps -ef'
+          }
+        }
+        stage('sub-job5'){
+          steps{
+            sh 'lsblk'
+          }
+        }
+      }
+    }
+    stage("Checkout Code") {
                steps {
                    git branch: 'main',
                           url: "${repoUrl}"
@@ -22,7 +47,6 @@ pipeline {
                    sh 'mvn -v'
                }
            }
-       }
+  }
 }
 }
-
