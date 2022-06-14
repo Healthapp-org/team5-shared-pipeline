2ea79c2bb14dc7fa1d6f45ac37c46b9e01f7403a
Pogun147 <105657287+Pogun147@users.noreply.github.com>
2022-06-14 08:10:50 -0400

    Update ozoonline.groovy

--- vars/ozoonline.groovy
+++ vars/ozoonline.groovy
@@ -1,1 +1,28 @@
+def call(String repoUrl){
+pipeline {
+       agent any
+       tools {
+           maven 'mymaven'
+       }
+       stages {
+           stage("Tools initialization") {
+               steps {
+                   sh 'mvn --version'
+                   sh 'java -version'
+               }
+           }
+           stage("Checkout Code") {
+               steps {
+                   git branch: 'main',
+                          url: "${repoUrl}"
+               }
+           }
+           stage("to-test-maven") {
+               steps {
+                   sh 'mvn -v'
+               }
+           }
+       }
+}
+}
 
